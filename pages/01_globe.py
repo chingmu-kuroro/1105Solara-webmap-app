c54b5dc771259eb598903edde3417d1d7cf7220e
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-05 08:39:26 +0000

    modified:   pages/01_globe.py

--- pages/01_globe.py
+++ pages/01_globe.py
@@ -16,6 +16,9 @@ def create_map():
     )
     m.add_basemap("Satellite", visible=False)
     m.add_overture_3d_buildings(template="simple")
+    lines_url = "https://github.com/opengeos/datasets/releases/download/vector/cables.geojson"
+    m.add_geojson(lines_url, name="Lines")
+
     return m
 
 
