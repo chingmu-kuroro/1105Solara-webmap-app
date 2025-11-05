aa416ef1dc1bb40b36f6a5df5be7971233f701d5
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-05 07:29:49 +0000

    modified:   pages/01_globe.py

--- pages/01_globe.py
+++ pages/01_globe.py
@@ -41,7 +41,7 @@ def Page():
         )
         
         # --- 4. 加入台北捷運路網 (向量) ---
-        mrt_url = "https://raw.githubusercontent.com/leoluyi/taipei_mrt/main/taipei-mrt.geojson"
+        mrt_url = "https://drive.google.com/file/d/1RwHIhfEINPFRYUCToMJzaOIEXLPNmZjX/view?usp=sharing"
         
         # (使用 add_geojson 是可以的，因為這個輕量物件也有這個輔助函式)
         paint = {"line-color": "#FFD700", "line-width": 3}
