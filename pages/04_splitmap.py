35d807a7741f4a4be7b4abc1bc2204b500842ab5
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-05 15:26:21 +0000

    modified:   pages/04_splitmap.py

--- pages/04_splitmap.py
+++ pages/04_splitmap.py
@@ -10,7 +10,6 @@ def create_map():
         right_layer="NLCD 2016 CONUS Land Cover",
         left_label="2001",
         right_label="2016",
-        center=[36.1, -114.9],
         zoom=10,
         )
     return m
