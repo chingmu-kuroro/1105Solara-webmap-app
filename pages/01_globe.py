a52a8a108dfa56dcb6c43259ecde6c23b50b7ef8
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-05 08:55:33 +0000

    modified:   pages/01_globe.py

--- pages/01_globe.py
+++ pages/01_globe.py
@@ -16,8 +16,12 @@ def create_map():
     )
     m.add_basemap("Satellite", visible=False)
     m.add_overture_3d_buildings(template="simple")
+
     lines_url = "https://drive.google.com/uc?id=1RwHIhfEINPFRYUCToMJzaOIEXLPNmZjX"
     m.add_geojson(lines_url, name="Lines")
+
+    points_url = "https://drive.google.com/uc?id=1hmx20mj3XhqF4MViL03WWUdNN4bYlHZL"
+    m.add_geojson(points_url, name="Points")
 
     return m
 
