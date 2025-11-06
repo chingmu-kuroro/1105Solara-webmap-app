695c4a2831b93a6cbed026ad19f2886903052627
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-06 01:11:28 +0000

    modified:   pages/01_globe.py 	deleted:    pages/02_sidebar.py 	deleted:    pages/03_overture.py 	deleted:    pages/04_splitmap.py

--- pages/01_globe.py
+++ pages/01_globe.py
@@ -8,7 +8,7 @@ def create_map():
         style="dark-matter",
         #projection="globe",
         center=[121.5654, 25.0330], # 台北 101
-        zoom=16,
+        zoom=30,
         pitch=60,
         bearing=-17,
         height="750px",
